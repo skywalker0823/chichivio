08c1f751b598b55a99f689f2e9b12f90d0840393
eddie <eddie@lacoal.net>
2023-03-10 15:29:27 +0800

    3/10 update

--- run.py
+++ run.py
@@ -13,6 +13,8 @@ if __name__ == '__main__':
 # About Docker running at local
 # docker image build -t chi_vio .
 # docker run -dp5000:5000 --name chi_vio_container chi_vio
+# Limit hardware edition:
+# docker run -dp5000:5000 --name chi_vio_container --cpus=1 --memory=512m chi_vio
 # open 127.0.0.1:5000
 
 # Development mode
