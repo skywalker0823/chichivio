d8f5e973dbfa1404028f02c86705dfd98aac3785
eddie <eddie@lacoal.net>
2023-05-24 14:06:11 +0800

    let me see the work flow first...

--- database/planet_scale.py
+++ database/planet_scale.py
@@ -1,26 +1,26 @@
-from dotenv import load_dotenv
-import os
-import MySQLdb
+# from dotenv import load_dotenv
+# import os
+# import MySQLdb
 
-load_dotenv()
+# load_dotenv()
 
-class DB:
-    def __init__(self) -> None:
-        self.connection = MySQLdb.connect(
-            host= os.getenv("HOST"),
-            user=os.getenv("USERNAME"),
-            passwd= os.getenv("PASSWORD"),
-            db= os.getenv("DATABASE"),
-            autocommit = True,
-            ssl_mode = "VERIFY_IDENTITY",
-            ssl      = {
-                "ca": "/etc/ssl/cert.pem"
-            }
-        )
-        self.cursor = self.connection.cursor()
+# class DB:
+#     def __init__(self) -> None:
+#         self.connection = MySQLdb.connect(
+#             host= os.getenv("HOST"),
+#             user=os.getenv("USERNAME"),
+#             passwd= os.getenv("PASSWORD"),
+#             db= os.getenv("DATABASE"),
+#             autocommit = True,
+#             ssl_mode = "VERIFY_IDENTITY",
+#             ssl      = {
+#                 "ca": "/etc/ssl/cert.pem"
+#             }
+#         )
+#         self.cursor = self.connection.cursor()
     
-    def is_connected(self):
-        if self.connection.open:
-            return True
-        else:
-            return False
+#     def is_connected(self):
+#         if self.connection.open:
+#             return True
+#         else:
+#             return False
