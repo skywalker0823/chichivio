dc498d4146804d87c4d848d677b3c90598809b1d
eddie <eddie@lacoal.net>
2023-06-02 11:59:04 +0800

    signup login basic finction is OK

---
+++ database/mongo.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+import os, dotenv
+
+dotenv.load_dotenv()
+
+class Mongo:
+    def __init__(self) -> None:
+        self.client = MongoClient(os.getenv('DB_CONNECTION_DATA'))
+
+    def get_user(self, username, password):
+        query = {"username": username,"password": password}
+        result = self.client["chi_vio_db"]["users"].find_one(query)
+        return result
+
+    def insert_user(self, username, password):
+        query = {"username": username,"password": password}
+        result = self.client["chi_vio_db"]["users"].insert_one(query)
+        return result        
