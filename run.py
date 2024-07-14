97b9f2853617af5c8b25784426d476e91a7499fd
pikxl <skywalker0823@hotmail.com>
2024-07-14 21:50:45 +0800

    gunicorn ok version(not pushed)

--- run.py
+++ run.py
@@ -5,13 +5,3 @@ app, socketio = create_app()
 if __name__ == '__main__':
     socketio.run(app, debug=True,port=5000,host='0.0.0.0')
 
-
-# K8S test
-# kubectl apply -f deployment.yaml
-# kubectl get deployments
-# kubectl apply -f service.yaml
-# kubectl get svc
-
-# DELETE
-# kubectl delete -f deployment.yaml
-# kubectl delete -f service.yaml
